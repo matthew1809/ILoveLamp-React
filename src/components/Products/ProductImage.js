f9d9ca944239473fb88c03d178c0a33f22c7dfc7
Matthew Foyle <matt@moltin.com>
2017-07-31 15:01:56 +0100

    refactor product image with functions to check for main_image and file

--- src/components/Products/ProductImage.js
+++ src/components/Products/ProductImage.js
@@ -3,34 +3,33 @@ import React, { Component } from 'react';
 class ProductImage extends Component {
 
   render() {
-    var placeholder = 'https://placeholdit.imgix.net/~text?txtsize=69&txt=824%C3%971050&w=824&h=1050';
-  
+    
     let file;
     let fileId;
-
-    try {
+    let placeholder = 'https://placeholdit.imgix.net/~text?txtsize=69&txt=824%C3%971050&w=824&h=1050';
+    
+    var isThereAMainImage = (product) => {
       fileId = this.props.product.relationships.main_image.data.id;
+      
       file = this.props.products.included.main_images.find(function (el) {
         return fileId === el.id
       })
 
-      return <img alt={'product name:' + this.props.product.name + ', product description:' + this.props.product.description} src={file.link.href} style={{"background": this.props.background}}/> || <img alt="placeholder" src={placeholder}/>
-
+      return <img alt={this.props.product.name + '-' + this.props.product.description} src={file.link.href} style={{"background": this.props.background}}/> || <img alt="placeholder" src={placeholder}/>
+    };
+    
+    var isThereAFile = (product) => {
+      fileId = this.props.product.relationships.files.data[0].id;
+      file = this.props.products.included.files.find(function (el) {
+        return fileId === el.id
+      })      
+        return <img alt={'product name:' + this.props.product.name + ', product description:' + this.props.product.description} src={file.link.href} style={{"background": this.props.background}}/> || <img alt="placeholder" src={placeholder}/>
+    };
+  
+    try {
+      return isThereAMainImage(this.props.product)
     } catch (e) {
-      
-      try {
-        fileId = this.props.product.relationships.files.data[0].id;
-        file = this.props.products.included.files.find(function (el) {
-          return fileId === el.id
-        })
-        
-          return <img alt={'product name:' + this.props.product.name + ', product description:' + this.props.product.description} src={file.link.href} style={{"background": this.props.background}}/> || <img alt="placeholder" src={placeholder}/>
-      
-      } catch (e) {
-      
-         return <img alt="placeholder" src={placeholder}/>
-         
-      }     
+      return isThereAFile(this.props.product) 
     }
   };
 };

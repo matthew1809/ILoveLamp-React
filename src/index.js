11f72a806f5518886f73116d40b94735acec87d3
Jamie Barton <hi@jamiebarton.co.uk>
2018-01-30 14:26:39 +0000

    chg: remove server worker import

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './store';
-import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
 ReactDOM.render(

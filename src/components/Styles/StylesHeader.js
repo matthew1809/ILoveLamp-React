6c88789fc2051af6cc17be82a6c9e9d349f90196
Jamie Barton <hi@jamiebarton.co.uk>
2018-02-02 12:16:46 +0000

    refactor: Remove Component import

--- src/components/Styles/StylesHeader.js
+++ src/components/Styles/StylesHeader.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import CartCounter from '../Cart/CartCounter';
 import { connect } from 'react-redux';

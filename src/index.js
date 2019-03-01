e39671c74cae4c5b7c4bfae64c66682335fdeba8
Tam Bui <itsmetambui@gmail.com>
2019-03-01 15:35:02 +0700

    update structure for now build script

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import store, { history } from './store';
 import App from './components/App';
 
 import './index.css';
+import './style.min.css';
 
 const target = document.getElementById('root');
 
